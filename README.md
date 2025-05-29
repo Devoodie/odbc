c956fb3162f50784ccbcfc94d9075b3f04f0ad34
Devoodie <devantecoley802@gmail.com>
2025-05-28 23:53:11 -0400

    TODO >> Add http support! Added CMakeLists.txt and sqlite library!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# odbc
+# ODBC
 This repository serves to teach me database interaction in C++.
+---
+# Compilation
+    - In order to compile this project the sqlite amalgamation files are needed place them into the sqlite directory.
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include "sqlite/sqlite3.h"
+
+int main() {
+	std::cout << "penis" << std::endl;
+	sqlite3 *db;
+
+	int status = sqlite3_open("../sqlite/database.db", &db);
+
+	if(status != SQLITE_OK){
+		std::cout << "failed to open database!" << std::endl;
+		exit(status);
+	}
+
+	sqlite3_close(db);
+}
