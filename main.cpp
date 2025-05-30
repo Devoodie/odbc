2b1c086c805300bd1335d21f6f7d594200926b9a
Devoodie <devantecoley802@gmail.com>
2025-05-29 20:49:43 -0400

    TODO >> Finish database initialization logic!

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <cstdlib>
+#include <sys/stat.h>
 #include <iostream>
+#include <fstream>
 #include "sqlite/sqlite3.h"
 
 int main() {
-	std::cout << "penis" << std::endl;
 	sqlite3 *db;
+	sqlite3_stmt *stmt;
 
-	int status = sqlite3_open("../sqlite/database.db", &db);
+	struct stat buffer;
+	bool db_exists;
+	
+	db_exists = !stat("../sqlite/database.db", &buffer);
 
-	if(status != SQLITE_OK){
+	int open_status = sqlite3_open("../sqlite/database.db", &db);
+
+	if(open_status != SQLITE_OK){
 		std::cout << "failed to open database!" << std::endl;
-		exit(status);
+		exit(open_status);
 	}
 
-	sqlite3_stmt *stmt;
+	if(!db_exists){
+		std::ifstream ifs; //work here
+		ifs.open("../sqlite/initialize_db.sql", std::ifstream::in);
+		if(ifs.bad()){
+			std::cerr << "IO Stream Error!" << std::endl;
+			exit();
+		};
+	}
 
 	sqlite3_close(db);
 }
