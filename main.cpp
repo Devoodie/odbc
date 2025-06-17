1ea7695169025d4706fad406966320d595beaa19
Devante Coley <devantecoley802@gmail.com>
2025-06-17 19:44:20 -0400

    TODO >> Write response using beast! was able to read a request using beast!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <boost/asio/registered_buffer.hpp>
-#include <boost/asio/write.hpp>
 #include <sys/stat.h>
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
+#include <boost/asio/registered_buffer.hpp>
+#include <boost/asio/write.hpp>
+#include <boost/beast.hpp>
 #include "include/initialization.h"
 #include "include/http_utils.h"
 
@@ -12,6 +13,7 @@ const char* db_path = "../sqlite/database.db";
 
 using namespace boost;
 using namespace boost::asio::ip;
+using namespace boost::beast;
 int main() {
 	//initialize sqlite db
 	sqlite3 *db;
@@ -39,29 +41,28 @@ int main() {
 
 	//initialize boost asio 
 
-	try {
 	asio::io_context io_context;
 	boost::system::error_code ec;
-//	handlers::http_handler handler(io_context);
 	tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8085));
+	boost::beast::flat_buffer read_buffer;
+	http::request<http::string_body> request;
 
-	std::string read_buffer;
+	try {
+		tcp::socket socket(io_context);
+		acceptor.accept(socket);
 
-	tcp::socket socket(io_context);
-	acceptor.accept(socket);
+		http::read(socket, read_buffer, request, ec);
 
-	std::string write_buffer = "HTTP/1.1 200 OK\nContent\nContent-Type: text/html; charset=UTF-8\nConnection: Closed\n\n<html><h1>Hello World! </h1></html>\n";
+		auto iterator = request.begin();
 
-	asio::write(socket, boost::asio::buffer(write_buffer), ec);
-
-	int bytes_read = asio::read(socket, asio::buffer(read_buffer));
-	std::cout << bytes_read << " bytes read!\n" << read_buffer << std::endl;
-	std::cout << read_buffer << std::endl;
+		while(iterator != request.end()){
+			std::cout << iterator->name_string() << ": " << iterator->value() << "\n";
+			iterator++;
+		}
 
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
-
 
 	sqlite3_close(db);
 }
