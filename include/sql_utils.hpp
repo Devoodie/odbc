6816fbed32d54c65492b929eefec6314e9630613
Devoodie <devantecoley802@gmail.com>
2025-06-27 20:27:55 -0400

    Added basic outline for function

--- include/sql_utils.hpp
+++ include/sql_utils.hpp
@@ -1,4 +1,5 @@
-#include <boost/uuid.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <string_view>
 namespace sql_utils{
-	;
+	boost::uuids::uuid GetUserSession(std::string_view user, std::string_view password);
 }
