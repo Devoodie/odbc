a78e6a85737f80612f84205a98bd89633a3c2cea
Devante Coley <devantecoley802@gmail.com>
2025-08-26 21:43:33 -0400

    TODO >> Add forking/rpc implementation for terraform!

---
+++ sqlite/test.sql
@@ -0,0 +1,11 @@
+INSERT INTO users(user_name, first_name, last_name, hash, salt)
+VALUES('Devooty', 'First_Name', 'Last_Name', X'2be82e3adda720ceda2e6b6c314e5f403f4b1e49a7657d1481a04abf99542e18', 'tfh36COCjzoqcRwD');
+
+INSERT INTO resources(vm_id, vm_name, owner)
+VALUES(100, 'TEST_VM', 1);
+
+INSERT INTO resources(vm_id, vm_name, owner)
+VALUES(101, 'TEST_VM_1', 1);
+
+INSERT INTO resources(vm_id, vm_name, owner)
+VALUES(101, 'TEST_VM_2', 1);
